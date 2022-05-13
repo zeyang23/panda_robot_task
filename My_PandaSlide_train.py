49eeed3d4ef331f96b77447fed38ae684a81c1e1
zeyang23 <zeyang23@outlook.com>
2022-05-13 20:18:14 +0800

    my panda slide

--- My_PandaSlide_train.py
+++ My_PandaSlide_train.py
@@ -5,7 +5,7 @@ import custom_envs
 
 env = gym.make('My_PandaPickAndPlace-v1')
 
-log_dir = './tensorboard/my_panda_pick_and_place_v1/'
+log_dir = './tensorboard/my_panda_slide_v1/'
 
 total_timesteps = 2000000
 
@@ -13,4 +13,4 @@ total_timesteps = 2000000
 model = SAC(policy="MultiInputPolicy", env=env, buffer_size=1000000, verbose=1, replay_buffer_class=HerReplayBuffer,
             tensorboard_log=log_dir)
 model.learn(total_timesteps=total_timesteps)
-model.save("./trained/my_panda_pick_and_place_v1/my_panda_pick_and_place_v1_sac")
+model.save("./trained/my_panda_slide_v1/my_panda_slide_v1_sac")
