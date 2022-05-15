06cdf15f963d57334af9c6b193f547e85c8ff8e8
zeyang23 <zeyang23@outlook.com>
2022-05-16 01:12:16 +0800

    remove some files

--- My_PandaSlideDense_train.py
+++ My_PandaSlideDense_train.py
@@ -4,11 +4,11 @@ from stable_baselines3 import SAC, HerReplayBuffer
 import custom_envs
 from sb3_contrib import TQC
 
-env = gym.make('My_PandaPickAndPlace-v1')
+env = gym.make('My_PandaSlide-v1')
 
 log_dir = './tensorboard/my_panda_slide_v1/'
 
-total_timesteps = 3000000
+total_timesteps = 2000000
 
 # SAC
 # model = SAC(policy="MultiInputPolicy", env=env, buffer_size=1000000, verbose=1, replay_buffer_class=HerReplayBuffer,
