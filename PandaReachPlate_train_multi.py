1d27b3fc1eaccb1e7286762e402633a52c426de4
zeyang23 <zeyang23@outlook.com>
2022-05-17 22:51:01 +0800

    train panda reach joints dense

--- PandaReachPlate_train_multi.py
+++ PandaReachPlate_train_multi.py
@@ -35,16 +35,16 @@ from typing import Callable
 #     return _init
 
 
-env_id = "My_PandaReachPlateDense-v1"
+env_id = "My_PandaReachPlateJointsDense-v1"
 num_cpu = 4
 vec_env = make_vec_env(env_id, n_envs=num_cpu)
 
-log_dir = './tensorboard/panda_reach_plate_dense_v1/'
+log_dir = './tensorboard/panda_reach_plate_joints_dense_v1/'
 
-total_timesteps = 1000000
+total_timesteps = 5000000
 
 # PPO
 model = PPO(policy="MultiInputPolicy", env=vec_env, verbose=1, normalize_advantage=True,
             tensorboard_log=log_dir)
 model.learn(total_timesteps=total_timesteps)
-model.save("./trained/panda_reach_plate_dense_v1/panda_reach_plate_dense_v1_ppo")
+model.save("./trained/panda_reach_plate_joints_dense_v1/panda_reach_plate_joints_dense_v1_ppo")
