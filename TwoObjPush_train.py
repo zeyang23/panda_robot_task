2966af9c4ab9813c3daf1f3b326d6ff3efdc2219
zeyang23 <zeyang23@outlook.com>
2022-05-19 23:24:07 +0800

    new training file

--- TwoObjPush_train.py
+++ TwoObjPush_train.py
@@ -2,8 +2,10 @@ import gym
 import panda_gym
 import custom_envs
 
-from stable_baselines3 import PPO, HerReplayBuffer
+from stable_baselines3 import PPO, SAC, HerReplayBuffer
 from sb3_contrib import TQC
+
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 from stable_baselines3.common.env_util import make_vec_env
 
@@ -30,12 +32,23 @@ total_timesteps = 3000000
 # model.learn(total_timesteps=total_timesteps)
 # model.save("./trained/two_obj_panda_push_dense_v1/two_obj_panda_push_dense_v1_ppo")
 
-env = gym.make("Two_Obj_PandaPush-v1")
+env = gym.make("Two_Obj_PandaPushDense-v1")
 
-model = TQC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
+checkpoint_callback = CheckpointCallback(save_freq=25000, save_path='model_checkpoints/two_obj_push',
+                                         name_prefix='two_obj_push')
+
+model = SAC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
             replay_buffer_class=HerReplayBuffer, policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
-            replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), gamma=0.95, tau=0.05,
+            replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future', ), gamma=0.95, tau=0.05,
             verbose=1,
             tensorboard_log=log_dir)
 model.learn(total_timesteps=total_timesteps)
-model.save("./trained/two_obj_panda_push_v1/two_obj_panda_push_v1_tqc")
+model.save("./trained/two_obj_panda_push_dense_v1/two_obj_panda_push_dense_v1_sac")
+
+# model = TQC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
+#             replay_buffer_class=HerReplayBuffer, policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
+#             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), gamma=0.95, tau=0.05,
+#             verbose=1,
+#             tensorboard_log=log_dir)
+# model.learn(total_timesteps=total_timesteps)
+# model.save("./trained/two_obj_panda_push_v1/two_obj_panda_push_v1_tqc")
--- TwoPush_train.py
+++ TwoPush_train.py
@@ -3,6 +3,7 @@ import panda_gym
 from stable_baselines3 import SAC, HerReplayBuffer
 import custom_envs
 from sb3_contrib import TQC
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 env = gym.make('Two_PandaPushDense-v1')
 
@@ -16,18 +17,21 @@ total_timesteps = 3000000
 # model.learn(total_timesteps=total_timesteps)
 # model.save("./trained/my_panda_slide_v1/my_panda_slide_v1_sac")
 
-# model = SAC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
-#             replay_buffer_class=HerReplayBuffer, policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
-#             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future', ), gamma=0.95, tau=0.05,
-#             verbose=1,
-#             tensorboard_log=log_dir)
-# model.learn(total_timesteps=total_timesteps)
-# model.save("./trained/my_panda_slide_v1/my_panda_slide_v1_sac")
+checkpoint_callback = CheckpointCallback(save_freq=100000, save_path='model_checkpoints/two_push',
+                                         name_prefix='two_push')
 
-model = TQC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
+model = SAC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
             replay_buffer_class=HerReplayBuffer, policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
-            replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), gamma=0.95, tau=0.05,
+            replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future', ), gamma=0.95, tau=0.05,
             verbose=1,
             tensorboard_log=log_dir)
 model.learn(total_timesteps=total_timesteps)
-model.save("./trained/two_push_dense_v1/two_push_dense_v1_tqc")
+model.save("./trained/two_push_dense_v1/two_push_dense_v1_sac")
+
+# model = TQC(policy="MultiInputPolicy", env=env, learning_rate=1e-3, buffer_size=1000000, batch_size=2048,
+#             replay_buffer_class=HerReplayBuffer, policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
+#             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), gamma=0.95, tau=0.05,
+#             verbose=1,
+#             tensorboard_log=log_dir)
+# model.learn(total_timesteps=total_timesteps)
+# model.save("./trained/two_push_dense_v1/two_push_dense_v1_tqc")
